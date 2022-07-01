53bfce8506546b96bfc08fe983fe60121b640043
eiru2224 <66399781+eiru2224@users.noreply.github.com>
2022-07-01 09:00:16 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+import dev.s7a.gradle.minecraft.server.tasks.LaunchMinecraftServerTask
+import dev.s7a.gradle.minecraft.server.tasks.LaunchMinecraftServerTask.JarUrl
+import groovy.lang.Closure
+import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
+
+plugins {
+    java
+    id("net.minecrell.plugin-yml.bukkit") version "0.5.1"
+    id("com.github.ben-manes.versions") version "0.41.0"
+    id("com.palantir.git-version") version "0.12.3"
+    id("dev.s7a.gradle.minecraft.server") version "1.2.0"
+}
+
+val gitVersion: Closure<String> by extra
+
+val pluginVersion: String by project.ext
+
+repositories {
+    mavenCentral()
+    maven(url = "https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven(url = "https://oss.sonatype.org/content/groups/public/")
+}
+
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:$pluginVersion-R0.1-SNAPSHOT")
+    compileOnly("org.jetbrains:annotations:23.0.0")
+}
+
+configure<BukkitPluginDescription> {
+    main = "@group@.Main"
+    version = gitVersion()
+    apiVersion = "1." + pluginVersion.split(".")[1]
+}
+
+task<LaunchMinecraftServerTask>("buildAndLaunchServer") {
+    dependsOn("build")
+    doFirst {
+        copy {
+            from(buildDir.resolve("libs/${project.name}.jar"))
+            into(buildDir.resolve("MinecraftServer/plugins"))
+        }
+    }
+
+    jarUrl.set(JarUrl.Paper(pluginVersion))
+    jarName.set("server.jar")
+    serverDirectory.set(buildDir.resolve("MinecraftServer"))
+    nogui.set(true)
+    agreeEula.set(true)
+}
+
+task<SetupTask>("setup")
